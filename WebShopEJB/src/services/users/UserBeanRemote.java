0fcdd9246c1629300824a1e79d50982afa8870fb
MilanRadeta <mikedoomsmith@hotmail.com>
2016-06-25 01:06:43 +0200

    JWT authentication progress libs in earcontent

--- WebShopEJB/src/services/users/UserBeanRemote.java
+++ WebShopEJB/src/services/users/UserBeanRemote.java
@@ -5,6 +5,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -15,6 +16,7 @@ public interface UserBeanRemote {
 
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
 	@Path("/login")
 	public Response login(User user);
 
